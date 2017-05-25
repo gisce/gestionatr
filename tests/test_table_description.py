49454b3b2a48d19a2dcef36c15d3c22a296988f6
Gerard Dalmau <gdalmau@gisce.net>
2017-05-25 10:05:19 +0200

    Fix get_description tests raising error

--- tests/test_table_description.py
+++ tests/test_table_description.py
@@ -12,10 +12,12 @@ class TestDefsTables(unittest.TestCase):
             get_description('02', 'TABLA_80')
         ).to(equal('Improcedente / Desfavorable'))
 
-        expect(
-            get_description('CODIGO_INVENTADO', 'TABLA_80')
-        ).to(raise_error(ValueError))
+        def get_non_existent_code():
+            return get_description('CODIGO_INVENTADO', 'TABLA_80')
 
-        expect(
-            get_description('01', 'TABLA_INVENTADA')
-        ).to(raise_error(ValueError))
+        expect(get_non_existent_code).to(raise_error(ValueError))
+
+        def get_non_existent_table():
+            return get_description('01', 'TABLA_INVENTADA')
+
+        expect(get_non_existent_table).to(raise_error(ValueError))
