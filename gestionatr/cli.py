783b04e40f3a03beee7e1ecd7357e73f32b0112d
tinogis <afita@gisce.net>
2018-07-24 08:32:29 +0200

    NEW sector as atr cli command

--- gestionatr/cli.py
+++ gestionatr/cli.py
@@ -4,6 +4,8 @@ import click
 
 from gestionatr.input.messages import message
 from gestionatr.input.messages import message_gas
+from gestionatr.input.messages.message import except_f1
+
 from gestionatr import __version__
 
 
@@ -15,29 +17,32 @@ def get_gestionatr_version(ctx, param, value):
         return
     click.echo(VERSION_TEXT)
     ctx.exit()
-    
+
 
 @click.group()
 @click.option("--version", "-v", is_flag=True, callback=get_gestionatr_version,
               expose_value=False, is_eager=True, help="ATR library version")
 def atr():
-    pass   
+    pass
 
 @atr.command(name='test')
 @click.option("--filename", "-f", help="path to XML filename", required=True)
-def test(filename):
+@click.option("--sector", "-s", help="e (power) or g (gas)", default="e")
+def test(filename, sector):
     with open(filename, 'r') as xml_file:
         try:
             data = xml_file.read()
-            m = message.Message(data)
+            if sector == 'e':
+                m = message.Message(data)
+            elif sector == 'g':
+                m = message_gas.MessageGas(data)
             m.parse_xml()
             sys.stdout.write(u'Correct File\n')
-        except Exception, e:
-            try:
-                m = message_gas.MessageGas(data)
-                m.parse_xml()
-                sys.stdout.write(u'Correct File\n')
-            except Exception, e:
-                sys.stdout.write(
-                    u'WARNING: Invalid File: {0}\n'.format(str(e.value))
-                )
+        except except_f1 as e:
+            sys.stdout.write(
+                u'WARNING: Invalid File: {0}\n'.format(str(e.value))
+            )
+        except Exception as e:
+            sys.stdout.write(
+                u'WARNING: Invalid File: {0}\n'.format(str(e))
+            )
