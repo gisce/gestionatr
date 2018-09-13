e3dedd8c2a1dba5d9b659f30d78a7352a9986933
polsala <polsalalidon@gmail.com>
2018-09-13 13:48:21 +0200

    Clas A1_04

---
+++ gestionatr/input/messages/A1_04.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from message_gas import MessageGas
+from Deadlines import ProcessDeadline, DeadLine, Workdays, Naturaldays
+from gestionatr.utils import get_rec_attr
+from A1_41 import A1_41
+
+
+class A1_04(A1_41):
+    """Clase que implementa A1_44."""
