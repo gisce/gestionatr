f9dabdcdcf340da59b1927a9e81b4fe71ad82a56
polsala <polsalalidon@gmail.com>
2018-09-20 13:52:19 +0200

    Avoid duplicated code with better inheritance

--- gestionatr/input/messages/A1_04.py
+++ gestionatr/input/messages/A1_04.py
@@ -1,25 +1,13 @@
 # -*- coding: utf-8 -*-
-from message_gas import MessageGas
-from Deadlines import ProcessDeadline, DeadLine, Workdays, Naturaldays
 from gestionatr.utils import get_rec_attr
-from A1_41 import A1_41
-from A1_44 import registerdoc
+from A1_44 import A1_44
 
 
-class A1_04(A1_41):
+class A1_04(A1_44):
     """Clase que implementa A1_44."""
     @property
     def cancelreason(self):
         tree = '{0}.cancelreason'.format(self._header)
-        data = get_rec_attr(self.obj, tree, False)
-        if data is not None and data is not False:
-            return data.text
-        else:
-            return False
-
-    @property
-    def operationnum(self):
-        tree = '{0}.operationnum'.format(self._header)
         data = get_rec_attr(self.obj, tree, False)
         if data is not None and data is not False:
             return data.text
@@ -34,13 +22,3 @@ class A1_04(A1_41):
             return data.text
         else:
             return False
-
-    @property
-    def registerdoclist(self):
-        data = []
-        obj = get_rec_attr(self.obj, self._header, False)
-        if (hasattr(obj, 'registerdoclist') and
-                hasattr(obj.registerdoclist, 'registerdoc')):
-            for d in obj.registerdoclist.registerdoc:
-                data.append(registerdoc(d))
-        return data
