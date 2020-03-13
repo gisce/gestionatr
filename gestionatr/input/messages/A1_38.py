99ff3b7d6dd5ddc8117997a2c14feb1bc9faa437
Eduard Berloso Clarà <eberloso@gisce.net>
2020-03-13 15:07:36 +0100

    Import a338

--- gestionatr/input/messages/A1_38.py
+++ gestionatr/input/messages/A1_38.py
@@ -64,5 +64,103 @@ class A1_38(A1_03):
             return False
 
     # Pas a3
+    @property
+    def province(self):
+        tree = '{0}.province'.format(self._header)
+        data = get_rec_attr(self.obj, tree, False)
+        if data is not None and data is not False:
+            return data.text
+        else:
+            return False
+
+    @property
+    def city(self):
+        tree = '{0}.city'.format(self._header)
+        data = get_rec_attr(self.obj, tree, False)
+        if data is not None and data is not False:
+            return data.text
+        else:
+            return False
+
+    @property
+    def zipcode(self):
+        tree = '{0}.zipcode'.format(self._header)
+        data = get_rec_attr(self.obj, tree, False)
+        if data is not None and data is not False:
+            return data.text
+        else:
+            return False
+
+    @property
+    def streettype(self):
+        tree = '{0}.streettype'.format(self._header)
+        data = get_rec_attr(self.obj, tree, False)
+        if data is not None and data is not False:
+            return data.text
+        else:
+            return False
+
+    @property
+    def street(self):
+        tree = '{0}.street'.format(self._header)
+        data = get_rec_attr(self.obj, tree, False)
+        if data is not None and data is not False:
+            return data.text
+        else:
+            return False
+
+    @property
+    def streetnumber(self):
+        tree = '{0}.streetnumber'.format(self._header)
+        data = get_rec_attr(self.obj, tree, False)
+        if data is not None and data is not False:
+            return data.text
+        else:
+            return False
+
+    @property
+    def portal(self):
+        tree = '{0}.portal'.format(self._header)
+        data = get_rec_attr(self.obj, tree, False)
+        if data is not None and data is not False:
+            return data.text
+        else:
+            return False
+
+    @property
+    def staircase(self):
+        tree = '{0}.staircase'.format(self._header)
+        data = get_rec_attr(self.obj, tree, False)
+        if data is not None and data is not False:
+            return data.text
+        else:
+            return False
+
+    @property
+    def floor(self):
+        tree = '{0}.floor'.format(self._header)
+        data = get_rec_attr(self.obj, tree, False)
+        if data is not None and data is not False:
+            return data.text
+        else:
+            return False
+
+    @property
+    def door(self):
+        tree = '{0}.door'.format(self._header)
+        data = get_rec_attr(self.obj, tree, False)
+        if data is not None and data is not False:
+            return data.text
+        else:
+            return False
+
+    @property
+    def atr(self):
+        tree = '{0}.atr'.format(self._header)
+        data = get_rec_attr(self.obj, tree, False)
+        if data is not None and data is not False:
+            return data.text
+        else:
+            return False
 
     # Pas a4
