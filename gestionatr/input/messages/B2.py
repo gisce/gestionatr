addc3e7aa694c215703f2f3b303c249e17055fb1
Emili García <egarcia@gisce.net>
2020-04-16 15:02:20 +0200

    Fixes B2 imput model
    Fixes B2 imput model

--- gestionatr/input/messages/B2.py
+++ gestionatr/input/messages/B2.py
@@ -1,15 +1,21 @@
 # -*- coding: utf-8 -*-
 from B1 import B1, DatosActivacionBaja
+from gestionatr.utils import get_rec_attr
 
 
 class B2(B1):
     """Classe que implementa B2."""
-
+    # Datos paso 05
+    @property
+    def datos_activacion_baja(self):
+        tree = '{0}.DatosActivacionBaja'.format(self._header)
+        sol = get_rec_attr(self.obj, tree, False)
+        if sol not in [None, False]:
+            return DatosActivacionBaja(sol)
+        else:
+            return False
 
 class DatosActivacionBaja(DatosActivacionBaja):
-
-    def __init__(self, data):
-        self.datos_activacion_baja = data
 
     @property
     def motivo(self):
