d20041116d0c4c4c0382464fd6a99fb9e92df314
Emili García <egarcia@gisce.net>
2020-08-26 14:49:43 +0200

    Adds user and password params for future use
    Adds user and password params for future use

--- gestionatr/cli.py
+++ gestionatr/cli.py
@@ -5,7 +5,7 @@ import click
 from suds.client import Client as SudsClient
 
 
-def request_p0(url, xml_file):
+def request_p0(url, user, password, xml_file):
     client = SudsClient(url=url, cache=None)
     if isinstance(xml_file, str):
         xml_str = xml_file
@@ -22,9 +22,11 @@ def cli():
 
 @cli.command()
 @click.option('-u', '--url', default='http://localhost', help=u'URL del webservice', show_default=True)
+@click.option('-s', '--user', help=u'User del webservice', show_default=True)
+@click.option('-p', '--password', help=u'Password del webservice', show_default=True)
 @click.option('-f', '--file', help=u'Fitxer P0 pas 01 per enviar', show_default=True)
-def sollicitar_p0(url, file):
-    res = request_p0(url, file)
+def sollicitar_p0(url, user, password, file):
+    res = request_p0(url, user, password, file)
     print res
 
 
