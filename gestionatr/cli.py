51885ea455fba12df964c198f01d31498295611e
Emili García <egarcia@gisce.net>
2020-08-28 11:21:57 +0200

    Adds user/password authentication and changes Client to zeep
    Adds user/password authentication and changes Client to zeep

--- gestionatr/cli.py
+++ gestionatr/cli.py
@@ -2,11 +2,13 @@
 
 import sys
 import click
-from suds.client import Client as SudsClient
+from zeep import Client
+from zeep.wsse.username import UsernameToken
 
 
 def request_p0(url, user, password, xml_file):
-    client = SudsClient(url=url, cache=None)
+    client = Client(url, wsse=UsernameToken(user or 'admin', password or 'admin'))
+
     if isinstance(xml_file, str):
         xml_str = xml_file
     else:
@@ -22,8 +24,8 @@ def cli():
 
 @cli.command()
 @click.option('-u', '--url', default='http://localhost', help=u'URL del webservice', show_default=True)
-@click.option('-s', '--user', help=u'User del webservice', show_default=True)
-@click.option('-p', '--password', help=u'Password del webservice', show_default=True)
+@click.option('-s', '--user', default='admin', help=u'User del webservice', show_default=True)
+@click.option('-p', '--password', default='admin', help=u'Password del webservice', show_default=True)
 @click.option('-f', '--file', help=u'Fitxer P0 pas 01 per enviar', show_default=True)
 def sollicitar_p0(url, user, password, file):
     res = request_p0(url, user, password, file)
