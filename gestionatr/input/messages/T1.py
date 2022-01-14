e5b39dc0b7b13e34a480662c57c77a053b1ed70c
PSala <polsalalidon@gmail.com>
2022-01-14 12:00:56 +0000

    more compat py

--- gestionatr/input/messages/T1.py
+++ gestionatr/input/messages/T1.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-
-from C2 import C2, Direccion
+from __future__ import absolute_import, unicode_literals
+from .C2 import C2, Direccion
 from gestionatr.utils import get_rec_attr
 
 
